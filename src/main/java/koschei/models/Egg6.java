29e983172641369898ee193fbf07e6c7672eab3f
Julia Hortova <lawrova.julia@gail.com>
2022-12-04 20:26:49 +0300

    commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    @Autowired
+    private Duck5 duck;
+
+    @Autowired
+    private Needle7 needle;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
